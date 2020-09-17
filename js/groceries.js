c967cbff91ba42b8616e6baaefe162d5b0514f27
magnetenstad <tenstad.magne@gmail.com>
2020-09-17 18:10:43 +0200

    Add filters

--- js/groceries.js
+++ js/groceries.js
@@ -1,58 +1,88 @@
 
-$(document).ready(function()
-{
-	readFile(document.getElementById("csv"), "assets/groceries/groceries.csv")
+var table;
+var table_head;
+var filter_key;
+var sort_index;
+
+$(document).ready(function() {
+	tableInit("assets/groceries/groceries.csv", document.getElementById("table"));
 });
 
-function instanceElement(_id, _innerHTML, _parent)
-{
-	var e = document.createElement(_id)
-	e.innerHTML = _innerHTML
-	_parent.appendChild(e)
-	return e
-}
-
-function readFile(_element, _path)
-{
+function tableInit(_path, _parent) {
 	var xhttp = new XMLHttpRequest();
 
-	xhttp.onreadystatechange = function()
-	{
-		if (this.readyState == 4 && this.status == 200)
-		{
-			var e = instanceElement("table", "", _element)
-			createTable(e, this.responseText)
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			table = arrayFromCSV(this.responseText);
+			table_head = table.shift()
+			tableCreate(_parent, table, table_head);
 		}
 	};
 	xhttp.open("GET", _path, true);
 	xhttp.send();
 }
 
-function createTable(_table, _csv)
-{
-	instanceElement("caption", "Groceries", _table)
-	var head = instanceElement("thead", "", _table)
-	var body = instanceElement("tbody", "", _table)
-	
+function arrayFromCSV(_csv) {
+	var result = [];
 	var lines = _csv.split("\n");
-	var headlines = lines[0].split(",")
-	
-	var row = instanceElement("tr", "", head)
 
-	for (var i = 0; i < headlines.length; i++)
-	{
-		instanceElement("th", headlines[i], row)
+	for (var i = 0; i < lines.length; i++) {
+		result.push(lines[i].split(","));
+	}
+	
+	return result;
+}
+
+function tableCreate(_parent, _table, _table_head) {
+	var t = instanceElement("table", "", _parent);
+	instanceElement("caption", "Groceries", t);
+	var head = instanceElement("thead", "", t);
+	var row = instanceElement("tr", "", head);
+
+	for (var i = 0; i < _table_head.length; i++) {
+		instanceElement("th", _table_head[i], row);
 	}
 
-	for (var i = 1; i < lines.length; i++)
-	{
-		var row = instanceElement("tr", "", body)
+	var body = instanceElement("tbody", "", t);
 
-		words = lines[i].split(",")
+	for (var i = 0; i < _table.length; i++) {
+		var row = instanceElement("tr", "", body);
 
-		for (var j = 0; j < words.length; j++)
-		{
-			instanceElement("th", words[j], row)
+		for (var j = 0; j < _table[i].length; j++) {
+			instanceElement("th", _table[i][j], row);
 		}
 	}
+}
+
+function instanceElement(_id, _innerHTML, _parent) {
+	var e = document.createElement(_id);
+	e.innerHTML = _innerHTML;
+	_parent.appendChild(e);
+	return e;
+}
+
+function tableFilter(_element) {
+	tableReplace(_element, table.filter(checkFilter));
+}
+
+function sortSort(_element) {
+	tableReplace(_element, [... table].sort(sortFunction));
+}
+
+function tableReplace(_element, _table){
+	_element.innerHTML = "";
+	tableCreate(_element, _table, table_head);
+}
+
+function sortFunction(a, b) {
+	if (a[sort_index] === b[sort_index]) {
+		return 0;
+	}
+	else {
+		return (a[sort_index] < b[sort_index]) ? -1 : 1;
+	}
+}
+
+function checkFilter(_array) {
+	return _array.includes(filter_key);
 }
