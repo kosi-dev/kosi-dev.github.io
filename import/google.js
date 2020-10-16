f229c09d4ec70368b5e4f85571fbe2e840049773
magnetenstad <tenstad.magne@gmail.com>
2020-10-16 16:16:50 +0200

    Update google.js

--- import/google.js
+++ import/google.js
@@ -1,10 +1,10 @@
 document.write(
 	'<!-- Global site tag (gtag.js) - Google Analytics -->\
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-177985622-1"></script>\
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-7YF6NW0FWG"></script>\
 	<script>\
 	window.dataLayer = window.dataLayer || [];\
 	function gtag(){dataLayer.push(arguments);}\
 	gtag("js", new Date());\
-	gtag("config", "UA-177985622-1");\
+	gtag("config", "G-7YF6NW0FWG");\
 	</script>'
 )
