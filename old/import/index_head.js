e25453a3abc58cb60d44475785fe872a2475d602
magnetenstad <tenstad.magne@gmail.com>
2021-02-08 01:44:36 +0100

    Remove old

--- old/import/index_head.js
+++
@@ -1,7 +0,0 @@
-
-<head>
-	<script src="google.js"></script>
-	<link rel="stylesheet" href="css/index.css">
-	<link rel="shortcut icon" type="image/x-icon" href="assets/index/icon.ico">
-	<title>kosi.dev</title>
-</head>
